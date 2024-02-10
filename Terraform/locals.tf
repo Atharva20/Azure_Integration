805c8e5155642f57b02c528acd42492d86c7a4a6
Atharva_Bhagade <batharva20@gmail.com>
2024-02-10 14:38:29 +0530

    commenting key-vault-local-values

--- Terraform/locals.tf
+++ Terraform/locals.tf
@@ -12,8 +12,8 @@ locals {
   # Client Storage Account Name
   storage_account_name                  = format("sa%s%s%s%s", "az", var.env, "sea", var.instance)               // saazdevsea01
   client_storage_account_url            = format("https://%s.blob.core.windows.net", local.storage_account_name) // https://saazdevsea01.blob.core.windows.net
-  storage_account_connectionstring_ref1 = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storageconnectionstring[0].id})"
-  storage_account_connectionstring_ref2 = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/%s/)", local.keyvault_name, "storageconnectionstring")
+  # storage_account_connectionstring_ref1 = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storageconnectionstring[0].id})"
+  # storage_account_connectionstring_ref2 = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/%s/)", local.keyvault_name, "storageconnectionstring")
 
   # FunctionApp Name
   functionapp_name                 = format("fa-%s-%s-%s-%s", "azint", var.env, "sea", var.instance) // fa-azureintegration-devv-sea-01
