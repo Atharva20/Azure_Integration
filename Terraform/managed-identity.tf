d36cda25e1e44a9e9458ab96534e3c08551f0416
Atharva_Bhagade <batharva20@gmail.com>
2024-02-12 19:49:00 +0530

    Update managed-identity.tf

--- Terraform/managed-identity.tf
+++ Terraform/managed-identity.tf
@@ -5,13 +5,6 @@ resource "azurerm_role_assignment" "fa-sa-contributor" {
   principal_id         = azurerm_windows_function_app.azureintegration[0].identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "la-sa-contributor" {
-  count                = var.deploy_tier ? 1 : 0
-  scope                = azurerm_storage_account.clientstorageaccount[0].id
-  role_definition_name = var.storage_access_role_defination
-  principal_id         = azurerm_logic_app_standard.testlogicapp[0].identity[0].principal_id
-}
-
 resource "azurerm_role_assignment" "fa-sb-owner" {
   count                = var.deploy_tier ? 1 : 0
   scope                = azurerm_servicebus_namespace.servicebus_namespace[0].id
@@ -19,9 +12,16 @@ resource "azurerm_role_assignment" "fa-sb-owner" {
   principal_id         = azurerm_windows_function_app.azureintegration[0].identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "la-sb-owner" {
-  count                = var.deploy_tier ? 1 : 0
-  scope                = azurerm_servicebus_namespace.servicebus_namespace[0].id
-  role_definition_name = var.servicebus_access_role_defination
-  principal_id         = azurerm_logic_app_standard.testlogicapp[0].identity[0].principal_id
-}
+# resource "azurerm_role_assignment" "la-sa-contributor" {
+#   count                = var.deploy_tier ? 1 : 0
+#   scope                = azurerm_storage_account.clientstorageaccount[0].id
+#   role_definition_name = var.storage_access_role_defination
+#   principal_id         = azurerm_logic_app_standard.testlogicapp[0].identity[0].principal_id
+# }
+
+# resource "azurerm_role_assignment" "la-sb-owner" {
+#   count                = var.deploy_tier ? 1 : 0
+#   scope                = azurerm_servicebus_namespace.servicebus_namespace[0].id
+#   role_definition_name = var.servicebus_access_role_defination
+#   principal_id         = azurerm_logic_app_standard.testlogicapp[0].identity[0].principal_id
+# }
