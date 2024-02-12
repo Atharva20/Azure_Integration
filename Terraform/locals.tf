87bde2c549024f23b6a19f470e9de5c75df2c6f2
Atharva_Bhagade <batharva20@gmail.com>
2024-02-12 19:27:28 +0530

    updated tfplan check

--- Terraform/locals.tf
+++ Terraform/locals.tf
@@ -10,7 +10,7 @@ locals {
   keyvault_name = format("kv-%s-%s-%s-%s", "azint", var.env, "sea", var.instance)
 
   # Client Storage Account Name
-  storage_account_name                  = format("sa%s%s%s%s", "az", var.env, "sea", var.instance)               // saazdevsea01
+  storage_account_name                  = format("sa%s%s%s%s", "azint", var.env, "sea", var.instance)               // saazintdevsea01
   client_storage_account_url            = format("https://%s.blob.core.windows.net", local.storage_account_name) // https://saazdevsea01.blob.core.windows.net
   # storage_account_connectionstring_ref1 = "@Microsoft.KeyVault(SecretUri=${azurerm_key_vault_secret.storageconnectionstring[0].id})"
   # storage_account_connectionstring_ref2 = format("@Microsoft.KeyVault(SecretUri=https://%s.vault.azure.net/secrets/%s/)", local.keyvault_name, "storageconnectionstring")
