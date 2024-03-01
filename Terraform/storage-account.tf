e2cc4d0cf33f4392c5c0f0b5efb1773fc6e1a2de
Atharva_Bhagade <batharva20@gmail.com>
2024-03-01 12:19:34 +0530

    pipeline deploy testing

--- Terraform/storage-account.tf
+++ Terraform/storage-account.tf
@@ -16,18 +16,18 @@ resource "azurerm_storage_account" "functionapp_storage_account" {
   account_replication_type = var.account_replication_type
 }
 
-resource "azurerm_storage_account" "clientstorageaccount" {
-  count                    = var.deploy_tier ? 1 : 0
-  name                     = local.storage_account_name
-  resource_group_name      = azurerm_resource_group.resourcegrp[0].name
-  location                 = azurerm_resource_group.resourcegrp[0].location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  identity {
-    type         = "UserAssigned"
-    identity_ids = [azurerm_user_assigned_identity.mi[0].id]
-  }
-}
+# resource "azurerm_storage_account" "clientstorageaccount" {
+#   count                    = var.deploy_tier ? 1 : 0
+#   name                     = local.storage_account_name
+#   resource_group_name      = azurerm_resource_group.resourcegrp[0].name
+#   location                 = azurerm_resource_group.resourcegrp[0].location
+#   account_tier             = var.account_tier
+#   account_replication_type = var.account_replication_type
+#   identity {
+#     type         = "UserAssigned"
+#     identity_ids = [azurerm_user_assigned_identity.mi[0].id]
+#   }
+# }
 
 # resource "azurerm_storage_container" "inboundshipmetdata" {
 #   count                 = var.deploy_tier ? 1 : 0
