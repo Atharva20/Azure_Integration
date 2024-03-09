d2b0ff4fecf1d1cfb1dca84a990e3990a9e7fb82
Atharva_Bhagade <batharva20@gmail.com>
2024-03-09 12:46:52 +0530

    testing logic app deployment

--- Terraform/appserviceplan.tf
+++ Terraform/appserviceplan.tf
@@ -7,11 +7,11 @@ resource "azurerm_service_plan" "fa-asp" {
   sku_name            = var.sku_name_func
 }
 
-# resource "azurerm_service_plan" "la-asp" {
-#   count               = var.deploy_tier ? 1 : 0
-#   name                = local.logic_appservice_name
-#   resource_group_name = azurerm_resource_group.resourcegrp[0].name
-#   location            = azurerm_resource_group.resourcegrp[0].location
-#   os_type             = var.os_type
-#   sku_name            = var.sku_name_logic
-# }
+resource "azurerm_service_plan" "la-asp" {
+  count               = var.deploy_tier ? 1 : 0
+  name                = local.logic_appservice_name
+  resource_group_name = azurerm_resource_group.resourcegrp[0].name
+  location            = azurerm_resource_group.resourcegrp[0].location
+  os_type             = var.os_type
+  sku_name            = var.sku_name_logic
+}
--- Terraform/storage-account.tf
+++ Terraform/storage-account.tf
@@ -1,11 +1,11 @@
-# resource "azurerm_storage_account" "logicapp_storage_account" {
-#   count                    = var.deploy_tier ? 1 : 0
-#   name                     = local.logic_storage_account_name
-#   resource_group_name      = azurerm_resource_group.resourcegrp[0].name
-#   location                 = azurerm_resource_group.resourcegrp[0].location
-#   account_tier             = var.account_tier
-#   account_replication_type = var.account_replication_type
-# }
+resource "azurerm_storage_account" "logicapp_storage_account" {
+  count                    = var.deploy_tier ? 1 : 0
+  name                     = local.logic_storage_account_name
+  resource_group_name      = azurerm_resource_group.resourcegrp[0].name
+  location                 = azurerm_resource_group.resourcegrp[0].location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+}
 
 resource "azurerm_storage_account" "functionapp_storage_account" {
   count                    = var.deploy_tier ? 1 : 0
