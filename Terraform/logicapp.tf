ab6195d271bc8552bbedc03b958158f357132f1c
Atharva_Bhagade <batharva20@gmail.com>
2024-03-09 13:23:36 +0530

    Update logicapp.tf

--- Terraform/logicapp.tf
+++ Terraform/logicapp.tf
@@ -11,7 +11,7 @@ resource "azurerm_logic_app_standard" "testlogicapp" {
     "FUNCTIONS_WORKER_RUNTIME"            = "node"
     "FUNCTIONS_EXTENSION_VERSION"         = "~4"
     "WEBSITE_NODE_DEFAULT_VERSION"        = "~18"
-    "WEBSITE_RUN_FROM_PACKAGE"            = "1"
+    # "WEBSITE_RUN_FROM_PACKAGE"            = "1"
     "client_storage_account_url"          = local.client_storage_account_url
     "servicebus_fullyqualified_namespace" = local.servicebus_fullyqualified_namespace
   }
